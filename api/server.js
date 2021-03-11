047b22afefa362c45325ebc3eeb1697398d6bc5d
jglez <jglez.cs@gmail.com>
2021-03-10 19:58:23 -0600

    Added and instantiated morgan middleware

--- api/server.js
+++ api/server.js
@@ -1,8 +1,17 @@
+// Import Express from node_modules
 const express = require('express');
 
+// Import Morgan network activity logger middleware
+const morgan = require('morgan')
+
+// Instantiate the server
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+// Enable Express to parse JSON bodies
+server.use(express.json())
+
+// Enable Morgan
+server.use(morgan('dev'))
 
 // global middlewares and the user's router need to be connected here
 
@@ -10,4 +19,5 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// Expose our server to the outer world
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// require your server and launch it
+// Import Server
+const server = require('./api/server')
+
+const port = 5000
+
+// Start server
+server.listen(port, () => {
+  console.log('GREAT SCOTT, IT\'S WORKING!!!!')
+  console.log(`Listening on port ${port}`)
+})
